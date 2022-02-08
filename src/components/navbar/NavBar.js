4642e3bdcbd78012280bff0202cc7c65e2c18429
Jorge Diaz <93jads@gmail.com>
2022-02-07 22:46:53 -0300

    Created NavBar component

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,21 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <div>
+            <h1>TODO<span>TECNOLOGIA</span></h1>
+        <nav>
+            <ul>
+                <li><a href="#">Celulares</a></li>
+                <li><a href="#">Desktops</a></li> 
+                <li><a href="#">Laptops</a></li>
+                <li><a href="#">Tablets</a></li>
+                <li><a href="#">Contáctanos</a></li>
+
+            </ul>
+        </nav>
+        </div>
+    )
+  }
+
+  export default NavBar
